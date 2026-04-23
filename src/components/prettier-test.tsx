1264927aefb3b9f886c6530256b35737d4f20bb8
Issam <issamAitOuahmane@gmail.com>
2026-04-23 02:28:13 +0100

    Test commit to verify husky, lint-staged with eslint and prettier

---
+++ src/components/prettier-test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PrettierTestComponent: React.FC = () => {
+  const testVar = "hello world";
+  console.log(testVar);
+  return <div>{testVar}</div>;
+};
+
+export default PrettierTestComponent;
